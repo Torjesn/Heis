cc6adb16684266e105c678873b129b1564419dab
Torje Nysaether <torjesn@stud.ntnu.no>
2020-02-13 12:17:45 +0100

    "added own files"

---
+++ source/elevatorCue.c
@@ -0,0 +1,158 @@
+#include "hardware.h"
+
+const int g_number_of_floors = 4;
+
+void remove_orders_current_floor(int floor, int cue_order_up[], int cue_order_down[], int cue_order_inside[]) {
+    decrement_array_over_limit(cue_order_up, cue_order_up[floor], g_number_of_floors);
+    decrement_array_over_limit(cue_order_down, cue_order_down[floor], g_number_of_floors);
+    decrement_array_over_limit(cue_order_inside, cue_order_inside[floor],g_number_of_floors);
+    
+    cue_order_up[floor] = 0;
+    cue_order_down[floor] = 0;
+    cue_order_inside[floor] = 0;
+}
+
+void decrement_array_over_limit(int array[], int limit, int length) {
+    for (int i = 0; i < length; ++i) {
+        if(array[i] > limit) --array[i];
+    }
+}
+
+void get_next_destination(int cue_order_up[],int cue_order_down[], int cue_order_inside[], int* p_destination) {
+    
+    for(int i = 0; i< g_number_of_floors; ++i) {
+        if (cue_order_inside[i] == 1) {
+            return i;
+            
+            decrement_array_over_limit(cue_order_inside, 0, g_number_of_floors);
+        }
+    }
+    for(int i = 0; i< g_number_of_floors; ++i) {
+        if (cue_order_up[i] == 1) {
+            return i;
+            decrement_array_over_limit(cue_order_up, 0, g_number_of_floors);
+            decrement_array_over_limit(cue_order_down, 0, g_number_of_floors);
+        }
+        if (cue_order_down[i] == 1) {
+            return i;
+            decrement_array_over_limit(cue_order_up, 0, g_number_of_floors);
+            decrement_array_over_limit(cue_order_down, 0, g_number_of_floors);
+        }
+    }
+    return 0;
+    //return 0 hvis den ikke kan ta 
+}
+
+
+//En potensiell bug er hvordan jeg representerer floor uten 
+int check_if_stop_floor(int floor, int cue_order_up[], int cue_order_down[], int cue_order_inside[], HardwareMovement motor_state ) {
+    if(
+        cue_order_inside[floor-1] 
+        || (cue_order_up[floor-1] && motor_state ==  HARDWARE_MOVEMENT_UP) 
+        || (cue_order_down[floor-1] && motor_state ==  HARDWARE_MOVEMENT_DOWN)) 
+    {       
+        remove_orders_current_floor(floor, cue_order_up, cue_order_down, cue_order_inside);
+        return 1;
+    }
+    else return 0;
+}
+
+int read_floor() {
+    for (int i = 1; i <= g_number_of_floors; ++i ) {
+        if(hardware_read_floor_sensor(i)) return i;
+    }
+    return 0;
+}
+
+
+//Kan smelte sammen disse tre til en funksjon
+/*void get_elevator_up_input(int *p_cue_count_outside, int cue_order_up[]) {
+    for (int i = 0; i < number_of_floors-1; ++i) {
+        if(hardware_read_order(i+1, HARDWARE_ORDER_UP)) {
+            if(cue_order_up[i] == 0) {
+                cue_order_up[i] = *p_cue_count_outside+1;
+                *p_cue_count_outside++;
+            }
+        }
+    }
+}
+    
+void get_elevator_down_input(int *p_cue_count_outside, int cue_order_down[]) {
+   for (int i = 1; i < number_of_floors; ++i) {
+        if(hardware_read_order(i+1, HARDWARE_ORDER_DOWN)) { 
+            if(cue_order_down[i] == 0) {
+                cue_order_down[i] = *p_cue_count_outside+1;
+                *p_cue_count_outside++;
+            }
+        }
+    }
+}   
+    
+void get_elevator_inside_input(int *p_cue_count_inside, int cue_order_inside[]) {  
+    for (int i = 0; i < number_of_floors; ++i) {
+        if(hardware_read_order(i+1, HARDWARE_ORDER_INSIDE)) {
+            if(cue_order_inside[i] == 0) {
+                cue_order_inside[i] = *p_cue_count_inside+1;
+                *p_cue_count_inside++;
+            }
+        }
+    }
+}*/
+
+void get_elevator_input( int *p_cue_count_outside, int *p_cue_count_inside, int cue_order_up[], int cue_order_down[], int cue_order_inside[]) {  
+    for (int i = 0; i < g_number_of_floors; ++i) {
+        if(hardware_read_order(i+1, HARDWARE_ORDER_UP)) {
+            if(cue_order_up[i] == 0) {
+                cue_order_up[i] = ++*p_cue_count_outside;
+            }
+        }
+
+        if(hardware_read_order(i+1, HARDWARE_ORDER_DOWN)) { 
+            if(cue_order_down[i] == 0) {
+                cue_order_down[i] = ++*p_cue_count_outside;
+            }
+        }
+        
+        if(hardware_read_order(i+1, HARDWARE_ORDER_INSIDE)) {
+            if(cue_order_inside[i] == 0) {
+                cue_order_inside[i] = ++*p_cue_count_inside;
+            }
+        }
+    }
+}
+
+typedef
+
+typedef struct 
+
+
+void driver() { //ja, den skal hete noe annet enn driver ;)
+
+    int cue_count_outside = 0;
+    int cue_count_inside = 0;
+    int cue_order_up[] = {0,0,0,0}; //kan eventuelt ha tre her, gjør aksessering enklere med fire
+    int cue_order_down[] = {0,0,0,0}; // -||-
+    int cue_order_inside[] = {0,0,0,0};
+    int destination = 0;
+    int current_floor = 0;
+    HardwareMovement motor_state = HARDWARE_MOVEMENT_STOP;
+
+
+    while(1) {
+        get_elevator_input(&cue_count_outside, cue_order_up);
+
+    }
+}
+
+
+
+
+
+
+void set_state(HardwareMovement * p_motor_state, int destination, int current_floor) { 
+    if (current_floor > destination) *p_motor_state = HARDWARE_MOVEMENT_DOWN;
+    else if (current_floor < destination) *p_motor_state = HARDWARE_MOVEMENT_UP;
+    else *p_motor_state = HARDWARE_MOVEMENT_STOP; //Jeg vet ikke helt hvordan dette vil fungere, hva hvis man trykker på den samme i 
+}
+
+void check_stop_signal() {}
---
+++ source/elevatorCue.h
@@ -0,0 +1,73 @@
+/**
+* @file
+* @brief The cue system of the elevator
+
+*/
+
+#include "hardware.h"
+
+/**
+ * @brief Removes the orders of the current floor, and decrement the cues
+ * @param[in,out] cue_order_up The up order cue with the current floor removed
+ * @param[in,out] cue_order_down The down order cue with the current floor removed
+ * @param[in,out] cue_order_inside The inside order cue with the current floor removed
+
+*/
+
+void remove_orders_current_floor(int floor, int cue_order_up[], int cue_order_down[], int cue_order_inside[]);
+
+
+
+/**
+ * @brief Decrements the values of an array over a chosen limit, used for manipulating cues
+ * @param[in,out] array The array being decremented
+ * @param[in] limit Numbers bigger than this limit is decremented by one in the array
+ * @param[in] length The size of the array
+
+*/
+void decrement_array_over_limit(int array[], int limit, int length);
+
+/**
+ * @brief Choses the next destination of the elevator. The inside cue is prioritized over the the outside ones
+ * @param[in,out] cue_order_up The up order cue decremented by one if next destination
+ * @param[in,out] cue_order_dowm The down order cue decremented bye one if next destination
+ * @param[in,out] cue_order_inside The inside order cue decremented by one if next destination
+ * @return next_flor Return the next floor, 0 if there are none requests pending
+ //Ikke skrevet ferdig
+*/
+
+void get_next_destination(int cue_order_up[],int cue_order_down[], int cue_order_inside[]);
+
+
+/**
+ * @brief Choses the next destination of the elevator. The inside cue is prioritized over the the outside ones
+ * @param[in] cue_order_up The up order cue 
+ * @param[in] cue_order_dowm The down order cue
+ * @param[in] cue_order_inside The inside order cue 
+ * @param[in] floor The current floor
+ * @param[in] motor_state The current direction of the motor
+ * @return stop Returns 1 if the motor should stop, 0 otherwise
+*/
+int check_if_stop_floor(int floor, int cue_order_up[], int cue_order_down[], int cue_order_inside[], HardwareMovement motor_state);
+
+/**
+ * @brief Polls input from harware and returns the current floor
+ * @return floor Returns the number of the floor, or 0 as default
+ //0 as default burde kanskje endres
+*/
+int read_floor();
+
+void get_elevator_input( int *p_cue_count_outside, int *p_cue_count_inside, int cue_order_up[], int cue_order_down[], int cue_order_inside[]);
+
+
+void get_elevator_up_input(int *p_cue_count_outside, int cue_order_up[]);
+
+
+/**
+ * @brief Sets the motor state based on destination and current floor
+ * @param[out] p_motor_state Reference to motor state, changed based on the other parameters
+ * @param[in] destination The next place the elevator is heading
+ * @param[in] current_floor The current floor measured by hardware
+ 
+*/
+void set_state(HardwareMovement * p_motor_state, int destination, int current_floor)
